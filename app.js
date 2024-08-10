368309fedc13790092f091e4ff112836f285a0b9
Jean-yun <yesirchloe@gmail.com>
2024-08-10 11:41:50 +0900

    add localstorage function to save user info

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const h1 = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
-  loginForm.classList.add("hidden");
-  h1.innerText = `Hello ${userName}`;
-  h1.classList.remove("hidden");
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+//Set the repetitive lines into function
+function paintGreetings(username) {
+  h1.innerText = `Hello ${username}`;
+  h1.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+//If name does not exist : show the form and get the item
+if (savedUserName === null) {
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
+
+//If name alrdy exist :show the greetings
+else {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(savedUserName);
+}
