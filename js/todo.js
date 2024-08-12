3b7455f984a7185d6c5ac8f39959c1fecfcd0b55
Jean-yun <yesirchloe@gmail.com>
2024-08-12 15:57:11 +0900

    added geolocation for weather and city name

--- js/todo.js
+++ js/todo.js
@@ -2,22 +2,27 @@ const todoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
 const todoInput = document.querySelector("#todo-form input");
 
-const toDos = [];
+//todos should store the previous values
+const TODOS_KEY = "todos";
+let toDos = [];
 
 function saveTodos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveTodos(toDos);
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   button.innerText = "❌";
   button.addEventListener("mousedown", deleteTodo);
   li.appendChild(span);
@@ -29,9 +34,22 @@ function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
-  toDos.push(newTodo);
-  paintTodo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj);
   saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+console.log(savedTodos);
+if (savedTodos) {
+  const parsedTodos = JSON.parse(savedTodos);
+  toDos = parsedTodos;
+  //to keep the todos saved in the website
+  parsedTodos.forEach(paintTodo);
+}
---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "4c735e830b62175d4ae2386e0c31c26a";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
